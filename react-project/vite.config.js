f80f5b1be48ed130bbd716e44f77c6e3bbf563ec
efoust11 <efoust11@ivytech.edu>
2025-09-21 19:03:46 +0000

    M5 Tutorial, deploy to pages

--- react-project/vite.config.js
+++ react-project/vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://efoust11.github.io/react-essential-training-5949338/react-project/",
 })
